41f7761e2d463b9a394e8bf669741b9cc7e9aa5f
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-30 16:37:00 +0200

    'main'

--- js/code.js
+++ js/code.js
@@ -148,11 +148,13 @@
 // }
 // ===========================================================================
 // Step-2 //
-let btnAddition = document.querySelector('button');
-btnAddition.addEventListener('click', ()=>{
-    let numb1 = document.querySelector("#numb1").value;
-    let numb2 = document.querySelector("#numb2").value;
-    document.querySelector('#output').innerText = 
-    parseInt(numb1) + parseInt(numb2);
+// let btnAddition = document.querySelector('button');
+// btnAddition.addEventListener('click', (e)=>{
+//     e.preventDefault();
+//     let numb1 = document.querySelector("#numb1").value;
+//     let numb2 = document.querySelector("#numb2").value;
+//     document.querySelector('#output').innerText = 
+//     parseInt(numb1) + parseInt(numb2);
 
-})
+// })
+// ===========================================================================
